002e102274e65c38d44bb3714ee07447bea8afe1
Pnkcaht <Samzoovsk19@gmail.com>
2026-01-06 19:02:05 -0500

    refactor: simplify IssueDecision type by removing optional reason fields

--- src/domain/issue/IssueDecision.ts
+++ src/domain/issue/IssueDecision.ts
@@ -1,19 +1,16 @@
 export type IssueDecision =
   | {
       action: "ignore";
-      reason?: string;
-    }
-  | {
-      action: "comment";
-      comment: string;
-      reason?: string;
-    }
-  | {
-      action: "label";
-      labels: string[];
-      reason?: string;
     }
   | {
       action: "close";
       reason: string;
+    }
+  | {
+      action: "comment";
+      comment: string;
+    }
+  | {
+      action: "label";
+      labels: string[];
     };
